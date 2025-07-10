a33d0417c7dc4fa12bc359f1136ed57e69588dff
mbportz <mbportuguez2@gmail.com>
2025-07-10 15:02:57 +0800

    added temporary screens for profiel and saved

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 
 const Profile = () => {
    return (
-      <View>
-         <Text>Profile</Text>
+      <View className="flex-1 bg-primary">
+         <Image source={images.bg} className="absolute w-full z-0" />
+
+         <View className="flex-1 justify-center items-center gap-y-5 ">
+            <Image source={icons.person} className="h-[50] w-[50]" />
+
+            <Text className="text-white text-center text-2xl">Profile</Text>
+         </View>
       </View>
    );
 };
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from "react";
+import { Image, Text, View } from "react-native";
+
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 
 const Saved = () => {
-  return (
-    <View>
-      <Text>Saved</Text>
-    </View>
-  )
-}
+   return (
+      <View className="flex-1 bg-primary">
+         <Image source={images.bg} className="absolute w-full z-0" />
 
-export default Saved
+         <View className="flex-1 justify-center items-center gap-y-5 ">
+            <Image source={icons.save} className="h-[50] w-[50]" />
+
+            <Text className="text-white text-center text-2xl">Saved</Text>
+         </View>
+      </View>
+   );
+};
+
+export default Saved;
